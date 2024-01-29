61c7ad0f4e97eb4834087da1626453a68796ea76
unknown <kitttipoom.put@gmail.com>
2024-01-29 22:42:03 +0700

    add: add asset and config color variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,30 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        firstOrange: "#E44A0C",
+        secondOrange: "#FF7037",
+        thirdOrange: "#FF986F",
+        fourthOrange: "#FFB899",
+        fifthOrange: "#FFD5C2",
+        sixthOrange: "##FFF1EC",
+        firstGray: "#3A3B46",
+        secondGray: "#5B5D6F",
+        thirdGray: "#7B7E8F",
+        fourthGray: "#AEB1C3",
+        fifthGray: "#DCDFED",
+        sixthGray: "#F6F6F9",
+        firstYellow: "#FFCA62",
+        secondYellow: "#FFF5EC",
+        firstLigthBlue: "#76D0FC",
+        secondLigthBlue: "#ECFBFF",
+        firstGreen: "#1CCD83",
+        secondGreen: "#E7FDF4",
+        firstPink: "#FA8AC0",
+        secondPink: "#FFF0F1",
+        firstRed: "#EA1010",
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
