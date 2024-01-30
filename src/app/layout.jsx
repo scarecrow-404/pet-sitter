c4745ea7c7248c5f2f29d08927135bed5aba5403
Sodikcp <paramesh.rah@gmail.com>
2024-01-30 18:23:35 +0700

    add: sitter detail page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
-
+import {Providers} from "./Provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Providers>{children}</Providers></body>
     </html>
   );
 }
