1dfe8a7542b0867ea2893873fa9bef0de58a2430
KIATTISAK PIMARPORN <enterpannet@gmail.com>
2024-01-30 12:39:12 +0700

    fear:HomePage sitter

--- src/app/sitter_management/page.jsx
+++ src/app/sitter_management/page.jsx
@@ -1,7 +1,51 @@
 import React from "react";
-
+import Image from "next/image";
+import {
+  FormControl,
+  FormLabel,
+ 
+  FormErrorMessage,
+  FormHelperText,
+} from "@chakra-ui/react";
+import Frame427321094 from "@/asset/images/Frame427321094.svg";
 const SitterManagement = () => {
-  return <div>this is sitter management page</div>;
+  return (
+    <div>
+      <div className="headBar flex">
+        <Image src="/profile.png" width={40} height={40} alt="profile" />
+        <div>Jane Maison</div>
+      </div>
+      <div className="Title flex justify-between">
+        <div className="nameTitle">Pet Sitter Profile</div>
+        <div>
+          <button className="bg-secondOrange rounded-3xl min-w-32 h-12">
+            Update
+          </button>
+        </div>
+      </div>
+      <div>
+        <div>Basic Information</div>
+        <div>
+          <p>Profile Image</p>
+          <Image src={Frame427321094} width={240} height={240} alt="profile" />
+        </div>
+        <div className="flex">
+          <div className="cl">
+            <label htmlFor="fullname">Your full name*</label>
+          <input type="text" className="fullname "/>
+          </div>
+          <div>
+            <FormLabel>Experience</FormLabel>
+            
+          </div>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
 };
 
 export default SitterManagement;
