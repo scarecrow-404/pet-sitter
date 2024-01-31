0a5a498fb11a805ee9840d08c15ea6a34195dd50
suphattraying <suphattra.spk@gmail.com>
2024-01-31 16:15:34 +0700

    feat:search sitterlist (render sitter list)

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/common/Footer";
 import Navbar from "@/components/common/Navbar";
+import {Providers} from './Provider'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,8 +13,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Providers>{children}</Providers></body>
     </html>
+   
   );
 }
