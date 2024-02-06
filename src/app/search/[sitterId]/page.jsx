748cf9be2ffb256229acda5800e8b4837668d54d
Sodikcp <paramesh.rah@gmail.com>
2024-02-06 17:20:40 +0700

    feat:test fetch data

--- src/app/search/[sitterId]/page.jsx
+++ src/app/search/[sitterId]/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 import avatar from "@/asset/images/secondSitterDetail.svg";
@@ -7,12 +7,33 @@ import avatar2 from "@/asset/images/firstSitterDetail.svg";
 import avatar3 from "@/asset/images/thirdSitterDetail.svg";
 import Carousel from "@/components/Carousel";
 import SitterDetail from "@/components/SitterDeatail";
-import PopupBooking from "@/components/Popup";
 import withAuth from "@/lib/utils/withAuth";
 import { useUser } from "@/hooks/hooks";
+import supabase from "@/lib/utils/db";
 const SitterProfile = () => {
   const { user, setUser } = useUser();
   const images = [{ url: avatar }, { url: avatar2 }, { url: avatar3 }];
+  // const [rating, setRating] = useState("");
+  const [detailUser, setDetailUser] = useState([]);
+
+  // const sitterRating = ["All Reviews", 5, 4, 3, 2, 1];
+
+  async function getSitterData() {
+    let { data, error } = await supabase
+      .from("pet_sitter")
+      .select("*,users(full_name)");
+
+    // .select(`id, sitter_name, district, province, users(full_name)`);
+    if (error || !data) {
+      console.log(error);
+    }
+    setDetailUser(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    getSitterData();
+  }, []);
 
   console.log(user);
   return (
@@ -28,7 +49,21 @@ const SitterProfile = () => {
       <div className="hidden lg:block md:hidden ">
         <Carousel images={images} picNum={3} />
       </div>
-      <SitterDetail />
+      <div>
+        {detailUser.map((item) => (
+          <SitterDetail
+            // id, sitter_name, district, province, users(full_name)
+            sitterName={item.sitter_name}
+            fullName={item.users.full_name}
+            exp={item.experience}
+            place={item.place}
+            service={item.service}
+            province={item.province}
+            introduction={item.introduction}
+            district={item.district}
+          />
+        ))}
+      </div>
 
       <Footer />
     </div>
