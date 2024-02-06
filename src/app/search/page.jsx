5ef4e85e93a8b153693d3a5876c07f58ddb35e48
suphattraying <suphattra.spk@gmail.com>
2024-02-06 17:09:33 +0700

    feat:search (fetch query data)

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -4,23 +4,46 @@ import { useState, useEffect } from "react";
 import CardSitter from "@/app/search/cardsitterlist.jsx";
 import SearchBar from "@/components/common/SearchBar";
 import { supabase } from "@/lib/db";
+import {useUser} from "@/hooks/hooks"
 
 import Navbar from "@/components/common/Navbar";
 import Footer from "@/components/common/Footer";
 
 const Search = () => {
+  const {search,setSearch}= useUser()
   const [sitterData, setSitterData] = useState([]);
   const [idSitter, setIdSitter] = useState("");
   const [pages, setPage] = useState(1);
+  const [expStart,setExpStart] = useState()
+  const [expEnd,setExpEnd] = useState()
 
-  // const getSitterList = async () => {
-  //   const result = await fetch("")
-  //   setSitterData(result)
-  // }
+  console.log("searchhhhhhhhh",search)
 
-  console.log(idSitter);
-  async function getSitterData() {
-    let { data, error } = await supabase.from("pet_sitter").select(`
+const expQuery = search.exp
+
+const petQuery = search.pet
+
+const ratingQuery = search.rating
+
+const splitExpNum = (num) => {
+  if(num){const split = num.split("-")
+    setExpStart(split[0])
+  setExpEnd(split[1])
+    }
+}
+
+
+console.log(idSitter)
+
+  async function getSitterData(expStart,expEnd, petQuery,ratingQuery){
+    console.log("fetchhhhhhhhhhhh")
+    console.log("expppppppp",expStart,expEnd)
+    console.log("petttttt",petQuery)
+    console.log("ratingggggg",ratingQuery)
+    if(!expStart && !expEnd && !petQuery && !ratingQuery){
+      console.log("1if")
+    let { data, error } = await supabase.from("pet_sitter")
+    .select(`
     id,
     sitter_name,
     district,
@@ -30,17 +53,38 @@ const Search = () => {
     if (error || !data) {
       console.log(error);
     }
-    // setSitterData(data);
-    // console.log(data)
+    setSitterData(data)
+    console.log("1eapppppppppppp",data)}
 
-    setSitterData(data);
-    // console.log(sitterData)
+ else if(expStart && expEnd && petQuery.length==0 && !ratingQuery){
+console.log("2if")
+    let { data, error } = await supabase.from("pet_sitter")
+    .select(`
+    id,
+    sitter_name,
+    district,
+    province,
+    experience,
+    users( full_name )
+  `).gt( "experience" , expStart).lte("experience" , expEnd)
+
+    if (error || !data) {
+      console.log(error);
+    }
+    setSitterData(data)
+    console.log("2eapppppppppppp",data)
   }
+}
 
-  useEffect(() => {
-    getSitterData();
-  }, []);
+// console.log(sitterData)
+  
 
+useEffect(()=>{
+  splitExpNum(expQuery)
+  getSitterData(expStart,expEnd, petQuery,ratingQuery) 
+
+},[search])
+ 
   function splitPage(numpage) {
     const pageArr = [];
 
@@ -51,13 +95,15 @@ const Search = () => {
   }
 
   return (
-    <section className=" w-full">
+    <section className=" max-w-[1440]">
       <Navbar />
 
-      <section className="  w-[100%] flex flex-col  gap-5 md:flex-row md:w-[80%] p-3">
+      <section className=" flex flex-col  lg:flex-row p-3">
+      <div className=" w-[100%] lg:w-[30%]">
         <SearchBar />
-        <CardSitter />
-        {/* {setSitterData.map((item,index)=>(
+        </div>
+   <div  className="  lg:w-[70%] w-[100%] flex flex-col gap-2">
+        {sitterData.map((item)=>(
         <CardSitter
        key={item.id}
        sittername={item.sitter_name}
@@ -65,13 +111,17 @@ const Search = () => {
       province = {item.province}
       fullname={item.users?.full_name}
       id={item.id}
-      
+   
        />
 
-      )})}
-      </diV>
-      </section>
+      ))}
+     
+      </div> 
 
+      
+
+    
+      </section>
       <div className="flex gap-2">
         <a href="#">pevious</a>
         {splitPage(10).map((item, index) => {
@@ -82,12 +132,11 @@ const Search = () => {
           );
         })}
         <a href="#">next</a>
-      </div>
-
+      </div> 
       <Footer />
-      </section>*/}
-      </section>
-    </section>
+     </section>
+      
+      
   );
 };
 
