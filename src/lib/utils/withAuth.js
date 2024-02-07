51dde21090ec3221bc49e8feb8a43046015ed55b
scarecrow-404 <kittipoom.put@gmail.com>
2024-02-07 17:47:34 +0700

    feat:add loading state of profile and userId context

--- src/lib/utils/withAuth.js
+++ src/lib/utils/withAuth.js
@@ -9,9 +9,14 @@ export default function withAuth(Component) {
     const router = useRouter();
     const [userSession, setUserSession] = useState(null);
     const { setUser, setUserId, userId } = useUser();
-    function getUser() {
-      const user = supabase.from("users").select("*").eq("id", userId);
-      setUser(user);
+    async function getUser(session) {
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", session.user.id);
+      setUser(data[0]);
+      setIsLoading(false);
+      return data[0]; // return the user data
     }
     useEffect(() => {
       const fetchSession = async () => {
@@ -23,13 +28,15 @@ export default function withAuth(Component) {
         setUserSession(session);
         setUser(session?.user);
         setUserId(session?.user?.id);
-        supabase.auth.onAuthStateChange((event, session) => {
+        supabase.auth.onAuthStateChange(async (event, session) => {
           console.log(`Supabase auth event: ${event}`);
           // console.log(session);
           if (event === "SIGNED_IN" || event === "INITIAL_SESSION") {
-            if (session) {
-              getUser();
-              //console.log(userId);
+            const currentSession = supabase.auth.session; // use supabase.auth.session, not supabase.auth.session()
+            if (currentSession) {
+              setUserId(currentSession.user.id);
+              const user = await getUser(currentSession);
+              setUser(user);
             }
           } else if (event === "SIGNED_OUT") {
             setUser(null);
