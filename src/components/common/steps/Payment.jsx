40d550bf47d67a333b7ed61efa85f2408485e6f8
Krit <titatit.akkh@gmail.com>
2024-02-12 14:06:39 +0700

    feat: add pet page and create validation

--- src/components/common/steps/Payment.jsx
+++ src/components/common/steps/Payment.jsx
@@ -3,34 +3,42 @@ import { useState } from "react";
 import CreditCard from "../payment/CreditCard";
 import Cash from "../payment/Cash";
 
-function Payment() {
-  const [changePage, setPage] = useState(true);
-
-  const creditCard = () => setPage(true);
-  const cash = () => setPage(false);
-
+function Payment({ values, handleInput, errors, handleClick }) {
   return (
-    <div className="flex flex-col justify-center">
+    <div className="flex flex-col justify-center ">
       <div>
-        <div className="flex mx-[20px] gap-[16px] ">
+        <div className="flex mx-[20px] lg:mx-[60px] gap-[16px] ">
           <button
-            className={`text-[12px] md:text-[18px] leading-[24px] h-[60px] md:h-[80px] w-full border-[4px] rounded-[999px]
-            ${changePage ? "text-[#FF7037] border-[#FF7037]" : ""}
+            className={`text-[12px] md:text-[18px] font-[500] leading-[26px] h-[60px] md:h-[80px] w-full border-[4px] rounded-[999px]
+            ${values.creditCard ? "text-[#FF7037] border-[#FF7037]" : ""}
             `}
-            onClick={!changePage ? creditCard : null}
+            value={values.creditCard}
+            onClick={handleClick}
           >
             Credit Card
           </button>
+
           <button
-            className={`text-[12px] md:text-[18px] leading-[24px] h-[60px] md:h-[80px] w-full border-[4px] rounded-[999px]
-            ${!changePage ? "text-[#FF7037] border-[#FF7037]" : ""}
+            className={`text-[12px] md:text-[18px] font-[500] leading-[26px] h-[60px] md:h-[80px] w-full border-[4px] rounded-[999px]
+            ${values.cash ? "text-[#FF7037] border-[#FF7037]" : ""}
             `}
-            onClick={changePage ? cash : null}
+            value={values.cash}
+            onClick={handleClick}
           >
             Cash
           </button>
         </div>
-        <div>{changePage ? <CreditCard /> : <Cash />}</div>
+        <div className="mx-[20px] lg:mx-[40px] ">
+          {values.creditCard ? (
+            <CreditCard
+              values={values}
+              handleInput={handleInput}
+              errors={errors}
+            />
+          ) : (
+            <Cash />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/common/steps/Pet.jsx
+++ src/components/common/steps/Pet.jsx
@@ -1,8 +1,97 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import Image from "next/image";
+import previewPet from "@/asset/images/catforsitterlist.jpg";
 
-function Pet() {
-  return <div className="">Pet</div>;
+const petData = [
+  {
+    id: 1,
+    name: "boy",
+    type: "dog",
+    img: previewPet,
+  },
+  {
+    id: 2,
+    name: "joe",
+    type: "cat",
+    img: previewPet,
+  },
+  {
+    id: 3,
+    name: "hawk",
+    type: "bird",
+    img: previewPet,
+  },
+  {
+    id: 4,
+    name: "bill",
+    type: "rabbit",
+    img: previewPet,
+  },
+];
+
+function Pet({ formData, handleInput }) {
+  const [petLists, setPetLists] = useState(petData);
+
+  const [isChecked, setChecked] = useState(false);
+
+  const handleClick = () => {
+    setChecked(!isChecked);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center md:items-start max-w-[1440px] mx-auto">
+      {/* topic */}
+      <div className="flex flex-row justify-center md:justify-start p-3">
+        <div className="text-[16px] font-[500] leading-[24px] md:ml-[60px] ">
+          Choose Your Pet
+        </div>
+      </div>
+      {/* area for map petlist */}
+      <div className="flex justify-center h-[480px] overflow-y-scroll w-full">
+        <div className="grid grid-cols-2 xl:grid-cols-3 py-5 gap-5">
+          {/* map here */}
+          {petData.map((pet) => {
+            return (
+              <div key={pet.id}>
+                <button
+                  onClick={handleClick}
+                  className={`border w-[140px] md:w-[200px] lg:w-[200px] xl:w-[240px] h-[240px] flex flex-col items-center gap-2 rounded-xl
+                ${isChecked ? "border border-[#FF7037]" : ""}`}
+                >
+                  <div className="flex flex-row justify-end w-full mr-[20px]">
+                    <input
+                      type="checkbox"
+                      name="price"
+                      id="price"
+                      checked={isChecked}
+                      className="w-[20px] h-[20px] rounded-[6px] text-[#FF7037] border-gray-300 mt-[10px] focus:ring-0"
+                    />
+                  </div>
+                  <div className="pet-photo">
+                    <Image
+                      src={pet.img}
+                      alt="preview-pet"
+                      className="rounded-full w-[110px] h-[110px]"
+                    />
+                  </div>
+                  <div className="text-[20px] font-[700] leading-[28px] ">
+                    {pet.name}
+                  </div>
+                  <div className="border-2 border-firstPink bg-secondPink text-firstPink mb-[15px] px-5 py-1 rounded-3xl font-medium">
+                    {pet.type}
+                  </div>
+                </button>
+              </div>
+            );
+          })}
+
+          {/* map end here */}
+          <div className="border w-[140px] md:w-[200px] lg:w-[220px] xl:w-[240px] h-[240px] flex flex-col items-center gap-2 rounded-xl"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Pet;
