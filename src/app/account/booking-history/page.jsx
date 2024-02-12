65c9913c7dd9b8faa685610473d791dc0c85c484
enterpannet <enterpannet@gmail.com>
2024-02-12 18:01:34 +0700

    edit petlist

--- src/app/account/booking-history/page.jsx
+++ src/app/account/booking-history/page.jsx
@@ -1,7 +1,22 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
+import Navbar from "@/components/common/Navbar";
+import { AccountBar } from "@/components/AccountBar";
+import BookingHistoryList from "@/components/bookingHistory";
 const BookingHistory = () => {
-  return <div>booking history</div>;
+  return (
+    <div className="max-w-[1440px] mx-auto bg-sixthGray gap-9">
+      <Navbar />
+      <div className="flex mt-5 ">
+        <div className="mx-5 hidden lg:block bg-white max-w-[250px] h-fit rounded-xl">
+          <AccountBar active={3} />
+        </div>
+        <div className="min-w-[375px] w-full bg-white rounded-xl md:ml-5 md:mr-5 md:mb-5 lg:ml-0">
+          <BookingHistoryList />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BookingHistory;
