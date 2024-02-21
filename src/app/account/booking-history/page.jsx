e49da004e1152c181911d280e3fd646d69b70368
Krit <titatit.akkh@gmail.com>
2024-02-21 19:59:11 +0700

    feat: try to fetch

--- src/app/account/booking-history/page.jsx
+++ src/app/account/booking-history/page.jsx
@@ -1,9 +1,63 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "@/components/common/Navbar";
 import { AccountBar } from "@/components/AccountBar";
-import BookingHistoryList from "@/components/BookingHistory";
+import supabase from "@/lib/utils/db";
+import { useUser } from "@/hooks/hooks";
+import BookingHistoryList from "@/components/bookingHistory";
+
 const BookingHistory = () => {
+  const [bookingDetail, setBookingDetail] = useState([]);
+  const [petSitterFullname, setPetSitterFullname] = useState([]);
+  const [petSitterImage, setPetSitterImage] = useState([]);
+  const [rating, setRating] = useState();
+  const [description, setDescription] = useState();
+  const [pet, setPet] = useState();
+  const [data, setData] = useState([]);
+  const { userId } = useUser();
+
+  let sitterId;
+  let bookingId;
+
+  function petName(arr) {
+    let allPetName = [];
+    arr.map((item) => {
+      allPetName.push(`${item.pet.name} `);
+    });
+    setPet(allPetName);
+  }
+
+  console.log(bookingDetail, "12");
+  console.log(petSitterFullname, "zxc");
+  console.log(petSitterImage, "zx");
+
+  useEffect(() => {
+    if (userId) {
+      fetchData(userId);
+    }
+  }, [userId]);
+
+  console.log(bookingDetail, "mmm");
+
+  async function fetchData(userId) {
+    try {
+      let { data: bookDetail, error: bookDetailError } = await supabase
+        .from("booking_detail")
+        .select(`*`)
+        .eq("user_id", userId);
+      if (bookDetail) {
+        console.log(bookDetail, "123");
+        setBookingDetail(bookDetail);
+      } else if (bookDetailError || !bookDetailError) {
+        console.log(bookDetailError);
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error.message);
+    }
+  }
+
+  console.log(bookingDetail, "fgh");
+
   return (
     <div className="max-w-[1440px] mx-auto bg-sixthGray gap-9">
       <Navbar />
@@ -12,7 +66,24 @@ const BookingHistory = () => {
           <AccountBar active={3} />
         </div>
         <div className="min-w-[375px] w-full bg-white rounded-xl md:ml-5 md:mr-5 md:mb-5 lg:ml-0">
-          <BookingHistoryList />
+          {bookingDetail.map((item) => {
+            console.log(item, "fvg");
+            <BookingHistoryList
+              key={item.booking_id}
+              sitterName={item.sitterName}
+              full_name={item.full_name}
+              created_at={item.created_at}
+              process_status={item.process_status}
+              booking_date={item.booking_date}
+              start_time={item.start_time}
+              end_time={item.end_time}
+              total_amount={item.total_amount}
+              img_url={item.img_url}
+              transaction_no={item.transaction_no}
+              booking_id={item.bookingId}
+              bookingDetail={bookingDetail}
+            />;
+          })}
         </div>
       </div>
     </div>
